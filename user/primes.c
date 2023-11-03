75bf1a313148063b69333781e122f395e4148bd9
echo-echo-chen <1611394830@qq.com>
2023-11-03 10:38:40 +0800

    finish lab1 and start lab2

--- user/primes.c
+++ user/primes.c
@@ -27,77 +27,86 @@
 //     if (p does not divide n)
 //         send n to right neighbor
 
-// int
-// main(int argc, char* argv[])
-// {
-//     int pleft[2];
-//     int pright;
-//     int num;
+void
+seive(int fd)
+{
+    int pid = fork();
 
-//     pipe(pleft);
-//     pright = pleft[0];
-//     for (int i = 2; i <= 35; i++) {
-//         write(pleft[1], &i, sizeof(i));
-//     }
-//     close(pleft[1]);
+    if (pid > 0) {
+        wait(0);
+        exit(0);
+    }
 
-//     while ((read(pright, &num, sizeof(num)) > 0)) {
-//         int cur_prime = num;
-//         printf("prime %d\n", num);
-//         pipe(pleft);
+    int prime;
+    if (read(fd, &prime, sizeof(prime)) == 0) {
+        exit(0);
+    }
 
-//         if (fork() > 0) {
-//             close(pleft[0]);
-//             while (read(pright, &num, sizeof(num)) > 0) {
-//                 if (num % cur_prime) {
-//                     write(pleft[1], &num, sizeof(num));
-//                 }
-//             }
-//             close(pleft[0]);
-//             close(pright);
-//             wait(0);
-//         } else {
-//             close(pleft[1]);
-//             pright = pleft[0];
-//         }
-//     }
-
-//     exit(0);
-// }
+    printf("prime %d\n", prime);
+    int p[2];
+    pipe(p);
+    int num;
+    while (read(fd, &num, sizeof(num))) {
+        if (num % prime) {
+            write(p[1], &num, sizeof(num));
+        }
+    }
+    close(p[1]);
+    // close(fd);
+    seive(p[0]);
+    exit(0);
+}
 
 int
 main(int argc, char* argv[])
 {
-    int p[2];
-    pipe(p);
+    int pleft[2];
 
-    for (int i = 2; i <= 35; ++i) {
-        write(p[1], &i, 1);
+    // 主进程不断向管道写入内容
+    pipe(pleft);
+    for (int i = 2; i <= 35; i++) {
+        write(pleft[1], &i, sizeof(i));
     }
-    close(p[1]);
 
-    int num;
-    int fd = p[0];
-    // close(fd);
+    close(pleft[1]);
+    seive(pleft[0]);
 
-    while ((read(fd, &num, 1)) > 0) {
-        int cur_prime = num;
-        printf("prime %d\n", cur_prime);
-        pipe(p);
-        if (fork() == 0) {
-            close(p[1]);
-            fd = p[0];
-        } else {
-            close(p[0]);
-            while ((read(fd, &num, 1)) > 0) {
-                if (num % cur_prime) {
-                    write(p[1], &num, 1);
-                }
-            }
-            close(p[1]);
-            close(fd);
-            wait(0);
-        }
-    }
     exit(0);
 }
+
+// int
+// main(int argc, char* argv[])
+// {
+//     int p[2];
+//     pipe(p);
+
+//     for (int i = 2; i <= 35; ++i) {
+//         write(p[1], &i, 1);
+//     }
+//     close(p[1]);
+
+//     int num;
+//     int fd = p[0];
+//     // close(fd);
+
+//     while ((read(fd, &num, 1)) > 0) {
+//         int cur_prime = num;
+//         printf("prime %d\n", cur_prime);
+//         pipe(p);
+//         if (fork() == 0) {
+//             close(p[1]);
+//             fd = p[0];
+//         } else {
+//             close(p[0]);
+//             while ((read(fd, &num, 1)) > 0) {
+//                 if (num % cur_prime) {
+//                     write(p[1], &num, 1);
+//                 }
+//             }
+//             close(p[1]);
+//             close(fd);
+//             wait(0);
+//         }
+//     }
+//     exit(0);
+// }
--- user/xargs.c
+++ user/xargs.c
@@ -5,8 +5,10 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-// 编写 xargs 工具，从标准输入读入数据，将每一行当作参数，加入到传给 xargs 的程序名和参数后面作为额外参数，然后执行。
-int main(int argc, char* argv[])
+// 编写 xargs 工具，从标准输入读入数据，将每一行当作参数，加入到传给 xargs
+// 的程序名和参数后面作为额外参数，然后执行。
+int
+main(int argc, char* argv[])
 {
     if (argc < 2) {
         printf("enter more args");
@@ -23,10 +25,12 @@ int main(int argc, char* argv[])
         } else {
             buf[i] = 0;
             int index = 0;
-            if (fork() == 0) { //为什么要用fork
+            if (fork() == 0) {          // 为什么要用fork
                 char* commands[MAXARG]; // 保存参数的数组
 
-                for (int k = 1; k < argc; k++) // 把命令行参数保存到参数数组中 比如 (echo 1 ; echo 2) | xargs -n 1 echo
+                for (int k = 1; k < argc;
+                     k++) // 把命令行参数保存到参数数组中 比如 (echo 1 ; echo 2)
+                          // | xargs -n 1 echo
                 {
                     commands[index++] = argv[k];
                 }
@@ -34,8 +38,10 @@ int main(int argc, char* argv[])
 
                 // 注意的是，如果exec函数执行成功则不会返回
                 // 这里两个参数分别指的是 可执行文件名称和所需传入的参数
-                // execl("/bin/sh", "sh", "-c", "echo byy | xargs echo", NULL);
-                exec(argv[1], commands); // 通常用于在一个进程中加载并执行另一个程序。它会用新的程序替代当前进程的镜像，即在执行新程序时，当前进程的代码和数据都会被新程序替代。
+                // execl("/bin/sh", "sh", "-c", "echo byy | xargs echo",NULL);
+                exec(
+                  argv[1],
+                  commands); // 通常用于在一个进程中加载并执行另一个程序。它会用新的程序替代当前进程的镜像，即在执行新程序时，当前进程的代码和数据都会被新程序替代。
             } else {
                 wait(0);
                 i = 0;
@@ -45,3 +51,36 @@ int main(int argc, char* argv[])
     }
     exit(0);
 }
+
+// void
+// main(int argc, char* argv[])
+// {
+//     char c;
+//     int i = 0;
+//     char buf[14];
+//     while (read(0, &c, sizeof(c))) {
+//         /**
+//          * 收集字符直到遇到换行符,并将之前的字符作为一个字符串
+//          */
+//         if (c != '\n') {
+//             buf[i++] = c;
+//         } else {
+//             int index = 0;
+//             if (fork() == 0) {
+//                 // child process
+//                 char* commands[MAXARG];
+//                 // 将xargs命令行参数传入到commands字符串数组中
+//                 for (int k = 1; k < argc; k++) {
+//                     commands[index++] = argv[k];
+//                 }
+//                 commands[2] = buf;
+//                 exec(argv[1], commands);
+//             } else {
+//                 wait(0);
+//                 i = 0;
+//                 memset(buf, 0, sizeof(buf));
+//             }
+//         }
+//     }
+//     exit(0);
+// }
